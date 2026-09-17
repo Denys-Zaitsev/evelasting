834282371518f03d84f348287449d8b1da940833
Denys Zaitsev <deniszaitsev536@gmail.com>
2026-09-17 08:35:45 +0200

    Keep player active across page navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,18 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar";
-import { PlayerProvider } from "./components/PlayerContext";
-import { LanguageProvider } from "./components/LanguageContext";
-import PerformanceMotionConfig from "./components/PerformanceMotionConfig";
 import Preloader from "./components/Preloader";
-import DeferredSoundCloudPlayer from "./components/DeferredSoundCloudPlayer";
 import HomeBelowFold from "./components/HomeBelowFold";
 
 export default function Home() {
   return (
-    <LanguageProvider>
-      <PlayerProvider>
-      <PerformanceMotionConfig>
+    <>
       <Preloader />
       <main id="main-content" className="min-h-screen overflow-x-clip bg-black text-white">
         <Navbar />
 
         <Hero />
         <HomeBelowFold />
-
-        <DeferredSoundCloudPlayer />
       </main>
-      </PerformanceMotionConfig>
-    </PlayerProvider>
-    </LanguageProvider>
+    </>
   );
 }
